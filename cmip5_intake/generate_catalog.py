7ada0601232742d4066bfa19d14af732082f0e0a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-11-01 16:48:44 -0600

    Add boilerplate code to generate intake catalog

---
+++ cmip5_intake/generate_catalog.py
@@ -0,0 +1,19 @@
+import yaml 
+from jinja2 import Environment, FileSystemLoader
+from pathlib import Path 
+import os 
+
+def generator(config_data_file_path, template_file_path):
+    # Load data from YAML into Python dictionary
+    config_data = yaml.load(open(config_data_file_path))
+ 
+    # Load Jinja2 template
+    template_dir = os.path.dirname(os.path.abspath(template_file_path))
+
+    env = Environment(loader=FileSystemLoader(template_dir), trim_blocks=True, lstrip_blocks=True)
+
+    template = env.get_template(os.path.basename(template_file_path))
+
+
+    # Render the template with data and print the output
+    print(template.render(config_data))
